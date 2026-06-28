aadd1d9411e173827cf458415154fab349bb73ac
Keshav Bhardwaj <bhardwajkeshav2007@gmail.com>
2026-06-28 17:33:57 +0530

    Add recursive functions and improve good_day
    Refactor good_day function to include default value for space parameter and add factorial, sum, and rem functions.

--- python/Function_Reccursion.py
+++ python/Function_Reccursion.py
@@ -1,11 +1,13 @@
-"""def good_day(name, ending, space = default value):
+#1st code
+def good_day(name, ending, space = "default value"):
     print("Good Day", name)
-    print(space)
     print(ending)
+    print(space)
     return "done" #return the specified value to any variable that calls the function, in this example it give "done" to a
 
 a = good_day("Harry", "Thanks",) #throws an error if all the required positional arguments are not present(only if the given parameter doesnt have a default value)
 
+#2nd code
 def factorial(n):
     if(n==1 or n==0):
       return 1
@@ -14,6 +16,7 @@ def factorial(n):
 n = int(input("Enter a Number: "))
 print("The factorial of",n,"is:",factorial(n))
 
+#3rd code
 def sum(n):
     if(n==1 or n==0):
       return 1
@@ -22,6 +25,7 @@ def sum(n):
 n = int(input("Enter a Number: "))
 print("The sum of",n,"is:",sum(n))
 
+#4th code
 def rem(l, word):
     n = []
     for items in l:
@@ -30,4 +34,3 @@ def rem(l, word):
     return n
 l = ["Rohan", "Harry", "sohan", "an"]
 print(rem(l, "an"))
-"""
