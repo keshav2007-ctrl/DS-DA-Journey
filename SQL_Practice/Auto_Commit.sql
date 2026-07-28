ca3bebe8b68b712be708465419f1183fb09373cf
Keshav Bhardwaj <bhardwajkeshav2007@gmail.com>
2026-07-28 16:42:56 +0530

    database related edits

--- SQL_Practice/Auto_Commit.sql
+++ SQL_Practice/Auto_Commit.sql
@@ -6,7 +6,7 @@ SET AUTOCOMMIT = 0;
 --if any mistakes were made we can use ROLLBACK to the last time we used COMMIT; undoing all the changes made.
 --FOR EXAMPLE:
 --DELETE FROM users WHERE employee_id = 1; this is the query we wanted to run but by mistake we ran it for id=2.
-DELETE FROM users WHERE employee_id = 2;
+DELETE FROM users WHERE id = 2;
 --ROLLBACK; can help us in rectifying this mistake and delete the actual employee we wanted to delete.
 ROLLBACK;
 --SET AUTOCOMMIT = 1; turns on autocommit again.
